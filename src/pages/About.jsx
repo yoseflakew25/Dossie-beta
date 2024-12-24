15e10ecc8fb0cff89d4016df6de6da39cb2962c9
yosef <jolarohe@gmail.com>
2024-12-24 10:37:42 +0300

    d

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,12 +10,10 @@ export default function About() {
         <div className="lg:bg-gray-50 lg:p-16 rounded-[4rem] space-y-6 md:flex md:gap-6 justify-center md:space-y-0 lg:items-center">
           <div className="mx-8 md:7/12 lg:w-1/2">
             <h2 className="text-3xl font-bold text-primary opacity-75 md:text-4xl ">
-            ሁሉንም ጨረታ በአንድ ቦታ!
-ከመላው ኢትዮጵያ በአንድ ቦታ።
+            IC’s Digital Record Management System
             </h2>
             <p className="my-8 text-gray-600">
-            With most comprehensive database of various tender opportunities in different business sectors, CheretaGate aims to level the playing field so that businesses of any size can have easy and affordable access to various contracting opportunities.
-
+            This system is designed exclusively for the Ethiopian Insurance Corporation to provide a seamless, secure, and efficient way to access and manage customer records. With our tailored solution, EIC can streamline operations and deliver superior service to its clients.
 
             </p>
 
